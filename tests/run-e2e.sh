da7e1af74e739b4868dc8e503baad565d1c0be99
Raj Zalavadia <rzalavad@redhat.com>
2024-02-12 18:11:48 +0530

    Added few extra logs (#12)
    * Added extra logs
    
    * Added sleep

--- tests/run-e2e.sh
+++ tests/run-e2e.sh
@@ -68,13 +68,23 @@ wait_for_operators_ready() {
 create_cr_for_anomaly_engine(){
 
     header "Configuring Anomaly Engine by creating CR"
-    # temp code 
-    pwd
-    echo "--------"
+
+    # temp code-----start------
     ls .
-    
+    echo "----------"
+    ls ./
+    echo "----------"
+    ls ./config/samples/
+    echo "----------"
+    # temp code-----end------
+
+    info "get anomalyengine"
+    kubectl get anomalyengine
+
+    info "create anomalyengine instance"
     kubectl apply -f ./config/samples/observability-analytics_v1alpha1_anomalyengine.yaml
     
+    sleep 30
     if ! kubectl -n "osa-anomaly-detection" get cronjob | grep "osa-anomaly-detection"; then
         fail "Cronjob not present to detect Anomaly"
         die
